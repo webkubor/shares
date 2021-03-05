6fc7f342afdae3cef1afdd09ea4d61d366427d4b
webkubor <webkubor@163.com>
2021-03-05 17:24:59 +0800

    element使用插件方式，按需引入配置

--- src/plugins/element3.js
+++ src/plugins/element3.js
@@ -1,7 +1,15 @@
-import element from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
+// 全局引入
+// import element from 'element-plus'
+// import 'element-plus/lib/theme-chalk/index.css'
+
+// 按需引入
+import {ElButton, ElInput} from 'element-plus'
+import 'element-plus/lib/theme-chalk/el-button.css'
+import 'element-plus/lib/theme-chalk/el-input.css'
+
 
 
 export default function(app) {
-  app.use(element)
+  app.use(ElButton)
+  app.use(ElInput)
 }
