eeb3bb9451fc32a2abfd088cd9326c8af069af2b
webkubor <webkubor@163.com>
2021-03-05 17:37:56 +0800

    加入layout布局

--- src/plugins/element3.js
+++ src/plugins/element3.js
@@ -1,15 +1,16 @@
 // 全局引入
-// import element from 'element-plus'
-// import 'element-plus/lib/theme-chalk/index.css'
+import element from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
 // 按需引入
-import {ElButton, ElInput} from 'element-plus'
-import 'element-plus/lib/theme-chalk/el-button.css'
-import 'element-plus/lib/theme-chalk/el-input.css'
+// import {ElButton, ElInput} from 'element-plus'
+// import 'element-plus/lib/theme-chalk/el-button.css'
+// import 'element-plus/lib/theme-chalk/el-input.css'
 
 
 
 export default function(app) {
-  app.use(ElButton)
-  app.use(ElInput)
+  // app.use(ElButton)
+  // app.use(ElInput)
+  app.use(element)
 }
