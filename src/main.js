7185a98fb71534f7c8c7c87d4ba52be6b74bcfed
webkubor <webkubor@163.com>
2021-04-01 11:42:07 +0800

    环境变量配置

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import element from 'plugins/element3'
 // 全局样式
 import 'styles/index.scss'
 
+
+console.log(import.meta.env.VITE_APP_ASSETS, import.meta.env.VITE_APP_CONTEXT, '.env') // 打印出 VITE_APP_ASSETS
+console.log(import.meta.env.VITE_APP_AGE, 'dev.local') // 打印出 dev.local
+console.log(import.meta.env.VITE_APP_NAME, 'dev') // 打印出 dev
+
 createApp(App)
 .use(router)
 .use(store)
--- src/router/routerMap.js
+++ src/router/routerMap.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: 王恩博 
+ * @Date: 2021-04-01 11:32:00 
+ * @Last Modified by: 王恩博
+ * @Last Modified time: 2021-04-01 11:32:46
+ * @懒加载 + 相对路劲的快捷访问配置
+ */
 
 const indexMap = [
   {
