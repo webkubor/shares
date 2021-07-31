74d466f3654c9ae7b8ea18b8a98abcf81f8ab5d8
webkubor <webkubor@163.com>
2021-07-31 19:28:43 +0800

    提交项目展示区域

--- src/plugins/nativeUI.js
+++ src/plugins/nativeUI.js
@@ -1,23 +1,32 @@
 /*
  * @Date: 2021-07-20 16:07:16
- * @LastEditTime: 2021-07-31 18:45:44
+ * @LastEditTime: 2021-07-31 19:18:50
  */
 import {
-  // create naive ui
   create,
-  // component
-  NButton
-} from 'naive-ui'
+  NButton,
+  NDialog,
+  NDivider,
+  NIcon,
+  NTag,
+  NEllipsis,
+  NCard,
+  NLayout,
+  NSkeleton,
+  NGrid,
+  NGridItem,
+} from "naive-ui";
 
 const naive = create({
-  components: [NButton]
-})
+  components: [NButton, NCard, NDialog, NGrid,
+    NGridItem, NDivider, NIcon, NTag, NEllipsis, NLayout,NSkeleton],
+});
 
 // 通用字体
-import 'vfonts/Lato.css'
+import "vfonts/Lato.css";
 // 等宽字体
-import 'vfonts/FiraCode.css'
+import "vfonts/FiraCode.css";
 
-export default function(app) {
-  app.use(naive)
+export default function (app) {
+  app.use(naive);
 }
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,13 +1,55 @@
 <!--
  * @Date: 2021-07-20 16:07:16
- * @LastEditTime: 2021-07-31 18:59:34
+ * @LastEditTime: 2021-07-31 19:20:17
 -->
 <template>
-  <n-button>naive-ui</n-button>
+  <n-grid :x-gap="12" :y-gap="8" :cols="4">
+    <n-grid-item>
+      <n-card title="带封面的卡片">
+        <template #cover>
+          <img
+            src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
+          />
+        </template>
+        卡片内容
+      </n-card>
+    </n-grid-item>
+    <n-grid-item>
+      <n-card title="带封面的卡片">
+        <template #cover>
+          <img
+            src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
+          />
+        </template>
+        卡片内容
+      </n-card>
+    </n-grid-item>
+    <n-grid-item>
+      <n-card title="带封面的卡片">
+        <template #cover>
+          <img
+            src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
+          />
+        </template>
+        卡片内容
+      </n-card>
+    </n-grid-item>
+     <n-grid-item>
+      <n-card title="带封面的卡片">
+        <template #cover>
+          <img
+            src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
+          />
+        </template>
+        卡片内容
+      </n-card>
+    </n-grid-item>
+  </n-grid>
 </template>
 
 <style lang="scss" scoped>
-.el-col {
-  border-radius: 4px;
+.n-card {
+  max-width: 800px;
+  max-height: 500px;
 }
 </style>
