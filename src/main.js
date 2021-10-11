33bb6154a6e82e1d117a2800395fb3c60f0088a9
webkubor <webkubor@163.com>
2021-10-11 16:26:45 +0800

    remove 去除遗留代码

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
 /*
  * @Date: 2021-07-20 16:07:16
- * @LastEditTime: 2021-10-11 16:23:32
+ * @LastEditTime: 2021-10-11 16:25:42
  */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import nativeUI from 'plugins/nativeUI'
 
-// 全局样式
+// gloabal
 import 'styles/index.scss'
 
-//自定义指令
 import copy from "./directivies/copy";
 
-// console.log(import.meta.env.VITE_APP_ASSETS, import.meta.env.VITE_APP_CONTEXT, '.env') // 打印出 VITE_APP_ASSETS
-// console.log(import.meta.env.VITE_APP_AGE, 'dev.local') // 打印出 dev.local
-// console.log(import.meta.env.VITE_APP_NAME, 'dev') // 打印出 dev
+
+console.log(import.meta.env.VITE_APP_NAME, 'dev args')
 
 createApp(App)
 .use(copy)
