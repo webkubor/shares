9db80c1b72d2102f14b69b66390984523780602d
webkubor <webkubor@163.com>
2021-10-22 13:53:39 +0800

    feat 布局调整

--- src/components/aboutLife.vue
+++ src/components/aboutLife.vue
@@ -31,16 +31,10 @@
       </div>
       <div class="right">
         <n-image-group>
-          <n-space>
-            <n-image
-              width="100"
-              src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
-            />
             <n-image
               width="100"
               src="https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"
             />
-          </n-space>
         </n-image-group>
       </div>
     </div>
