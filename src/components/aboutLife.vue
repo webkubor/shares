42c743cbd2596ad9d63a333c01ccc9cb41303c03
webkubor <webkubor@163.com>
2022-03-27 14:49:49 +0800

    构建生产环境打包

--- src/components/aboutLife.vue
+++ src/components/aboutLife.vue
@@ -1,18 +1,27 @@
 <template>
-  <n-divider title-placement="center">
-    关于我近期的生活
-    </n-divider>
+  <n-divider title-placement="center"> 关于我近期的生活 </n-divider>
   <div class="mylife">
-    <iframe
-      class="yuque"
-      src="https://www.yuque.com/webkubor"
-      frameborder="0"
-    ></iframe>
-    <iframe
-      class="weibo"
-      src="https://www.yuque.com/webkubor/life"
-      frameborder="0"
-    ></iframe>
+    <n-grid
+      cols="1 s:1 m:1 l:2 xl:2 2xl:2"
+      x-gap="15"
+      y-gap="15"
+      responsive="screen"
+    >
+      <n-grid-item>
+        <iframe
+          class="yuque"
+          src="https://www.yuque.com/webkubor"
+          frameborder="0"
+        ></iframe>
+      </n-grid-item>
+      <n-grid-item>
+        <iframe
+          class="weibo"
+          src="https://www.yuque.com/webkubor/life"
+          frameborder="0"
+        ></iframe>
+      </n-grid-item>
+    </n-grid>
   </div>
 </template>
 <script>
@@ -32,11 +41,12 @@ export default {
   padding: 10px;
   font-size: 20px;
   .yuque {
-    width: 50vw;
+    width: 100%;
     height: 800px;
   }
   .weibo {
-    flex: 1;
+    width: 100%;
+    height: 800px;
   }
 }
 </style>
