76d478ba3a9479683d188e5b5d5dbcde85128ce4
webkubor <webkubor@163.com>
2022-09-21 11:38:05 +0800

    构建生产环境打包

---
+++ dist/assets/img/index.a9faf15b.js
@@ -0,0 +1,1 @@
+import{c as t,u as r,o as n,r as s}from"./index.5b54f541.js";const p={__name:"index",setup(c){let{initTheme:e}=r();return e(),(_,a)=>{const o=s("router-view");return n(),t(o)}}};export{p as default};
