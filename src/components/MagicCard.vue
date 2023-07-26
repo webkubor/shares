3b4c7b17add75a489de313dccbd1532958590e91
webkubor <webkubor@163.com>
2023-07-26 15:01:46 +0800

    feat 移除无关DOM层级

--- src/components/MagicCard.vue
+++ src/components/MagicCard.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="card" ref="cardRef" @mouseover="handleMouseOver" @mouseout="handleMouseOut">
-    <div class="content">
       <slot></slot>
-    </div>
   </div>
 </template>
 
