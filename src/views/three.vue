37f36c776fcf98d55b09b6d74be3fa2aed9392ad
webkubor <webkubor@163.com>
2023-08-09 17:27:26 +0800

    feat 删除无用逻辑

--- src/views/three.vue
+++ src/views/three.vue
@@ -35,19 +35,16 @@ watchEffect(() => {
 });
 
 
-	/**
-	 * @description 渲染
-	 */
-     function renders() {
-		renderer.clear();
-		renderer.render( scene, camera );
-	}
+function renders() {
+    renderer.clear();
+    renderer.render(scene, camera);
+}
 
 function onWindowResize() {
     camera.aspect = innerWidth / innerHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize( innerWidth, innerHeight );
-		renders();
+    camera.updateProjectionMatrix();
+    renderer.setSize(innerWidth, innerHeight);
+    renders();
 }
 
 
@@ -63,7 +60,6 @@ function initRender() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio); // 设置屏幕像素比例
     canvasContainer.value.appendChild(renderer.domElement);
-
 }
 
 /**
@@ -78,31 +74,35 @@ function initScene() {
 }
 
 
- /**
-  * @description 初始化交互
-  * @return {*}
-  */
- function initControls() {
-  // 创建一个OrbitControls对象，并传入相机和渲染器的domElement作为参数
-  controls = new OrbitControls(camera, renderer.domElement);
+/**
+ * @description 初始化交互
+ * @return {*}
+ */
+function initControls() {
+    // 创建一个OrbitControls对象，并传入相机和渲染器的domElement作为参数
+    controls = new OrbitControls(camera, renderer.domElement);
 
-  // 启用阻尼效果，使相机运动更平滑
-  controls.enableDamping = true;
+    // 启用阻尼效果，使相机运动更平滑
+    controls.enableDamping = true;
 
-  // 允许缩放操作
-  controls.enableZoom = true;
+    // 允许缩放操作
+    controls.enableZoom = true;
 
-  // 是否自动旋转场景
-  controls.autoRotate = false;
+    // 是否自动旋转场景
+    controls.autoRotate = false;
 
-  // 自动旋转的速度
-  controls.autoRotateSpeed = 2;
+    // 自动旋转的速度
+    controls.autoRotateSpeed = 2;
 
-  // 允许平移操作
-  controls.enablePan = true;
+    // 允许平移操作
+    controls.enablePan = true;
 }
 
 
+/**
+ * @name 配置摄像机 
+ * @return {*}
+ */
 function initCamera() {
     // 第一个参数为摄像机视锥体垂直视野角度，第二个参数为摄像机视锥体长宽比，第三个参数为摄像机视锥体近端面，第四个参数为摄像机视锥体远端面
     camera = new THREE.PerspectiveCamera(
@@ -112,12 +112,14 @@ function initCamera() {
         1000
     );
     // 设置相机位置，对应参数分别表示x，y，z位置
-    // camera.position.z = 6;
     camera.position.set(0, 0, 6);
 }
 
 
-
+/**
+ * @name 初始化地球平铺图 
+ * @return {*}
+ */
 function initEarth() {
     const geometry = new THREE.SphereGeometry(planetSize.value, 32, 32);
     const textureLoader = new THREE.TextureLoader();
@@ -130,39 +132,12 @@ function initEarth() {
     scene.add(planet);
 }
 
-function initEarth2() {
-    // 创建TextureLoader实例
-    const globeTextureLoader = new THREE.TextureLoader();
-
-    // 地球半径
-    const radius = planetSize.value;
-    const group = new THREE.Group();
-    // 使用globeTextureLoader加载地球的贴图
-    globeTextureLoader.load('https://webjunjun.github.io/wechat/earth.jpg', function (texture) {
-        // 创建地球的几何体
-        const globeGeometry = new THREE.SphereGeometry(radius, 100, 100);
-
-        // 创建地球的材质，使用MeshStandardMaterial，其中map属性为地球贴图
-        const globeMaterial = new THREE.MeshStandardMaterial({ map: texture });
-
-        // 创建地球的网格，将几何体和材质组合在一起
-        const globeMesh = new THREE.Mesh(globeGeometry, globeMaterial);
-
-        // 设置地球的旋转角度，使用欧拉角来表示旋转
-        group.rotation.set(0.5, 2.9, 0.1);
-
-        // 将地球网格添加到group中
-        group.add(globeMesh);
-
-        // 将group添加到场景中
-        scene.add(group);
-
-    })
-
-}
 
 
-
+/**
+ * @name 创建一束光线 
+ * @return {*}
+ */
 function createLight() {
     // 创建一束光线
     // const color = 0xFFFFFF;// 光颜色
