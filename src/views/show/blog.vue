36ae83dfe4b9f283e9ad74f3ae5f176a81b291dd
webkubor <webkubor@163.com>
2023-09-07 14:51:29 +0800

    feat 等额本息贷款

--- src/views/show/blog.vue
+++ src/views/show/blog.vue
@@ -3,12 +3,12 @@
     <n-card title="贷款计算器">
       <n-form label-placement="left" label-width="auto">
         <n-form-item label="贷款金额">
-          <n-input-number v-model:value="model.loanAmount" :parse="parse"  :format="format"  show-count clearable />
+          <n-input-number v-model:value="model.loanAmount" :parse="parse" :format="format" show-count clearable />
         </n-form-item>
         <n-form-item label="年利率">
           <n-input-number v-model:value="model.annualInterestRate" maxlength="30" show-count clearable />
         </n-form-item>
-        <n-form-item label="总贷款-年数">
+        <n-form-item label="期限(年)">
           <n-input-number v-model:value="model.years" maxlength="30" show-count clearable />
         </n-form-item>
       </n-form>
@@ -16,23 +16,56 @@
     <div class="border-box">
       <div class="info-item">
         <h3>等额本息贷款</h3>
-        <div>总偿还: {{ equalPrincipalPayments.totalPayment }} 元</div>
-        <div>总利息: {{ equalPrincipalPayments.totalInterest }} 元</div>
-        <div>总偿还: {{ equalPrincipalPayments.totalMonths }} 月</div>
-      </div>
-      <div class="info-item">
-        <h3>等额本金贷款</h3>
+        <p>
+          每月偿还固定金额，包括本金和利息。
+          初期还款主要是利息，后期逐渐减少，本金逐渐增加。
+        </p>
+        <div>每月偿还: {{ equalInstallmentPayments.monthlyPayment }} 月</div>
         <div>总偿还: {{ equalInstallmentPayments.totalPayment }} 元</div>
         <div>总利息: {{ equalInstallmentPayments.totalInterest }} 元</div>
-        <div>总偿还: {{ equalInstallmentPayments.totalMonths }} 月</div>
+
+        <n-card>
+          贷款金额（P）：100,000 元
+          <br>
+          年利率（r）：5%（0.05）
+          <br>
+          贷款期限（n）：5 年（60 个月）
+          <br>
+          月利率 (r) = 年利率 / 12 = 0.05 / 12 ≈ 0.00417
+          <br>
+          Total Payment = P * [r(1 + r)^n] / [(1 + r)^n – 1]
+          = 100,000 * [0.00417(1 + 0.00417)^60] / [(1 + 0.00417)^60 – 1]
+          ≈ 113,227.40 元
+          <br>
+          Monthly Payment = Total Payment / n
+          = 113,227.40 / 60
+          ≈ 1,887.12 元
+          <br>
+          Total Interest = Total Payment - P
+          = 113,227.40 - 100,000
+          ≈ 13,227.40 元
+        </n-card>
       </div>
+      <!-- <div class="info-item">
+        <h3>等额本金贷款</h3>
+        <p>
+          月利息 = 剩余未偿还本金 × 月利率
+        </p>
+        <p>
+          每月偿还固定的本金，利息随着本金减少而减少。
+          总利息相对较低，但初期每月还款额较高
+        </p>
+        <div>每月偿还: {{ equalPrincipalPayments.monthlyPayment }} 元</div>
+        <div>总偿还: {{ equalPrincipalPayments.totalPayment }} 元</div>
+        <div>总利息: {{ equalPrincipalPayments.totalInterest }} 元</div>
+      </div> -->
     </div>
 
   </n-space>
 </template>
 <script lang="ts" setup>
 import { reactive, computed } from 'vue';
-import Big from 'big.js';
+import _ from 'lodash'
 
 let model = reactive({
   loanAmount: 100000,
@@ -40,40 +73,35 @@ let model = reactive({
   annualInterestRate: 0.05,
 })
 
-let monthlyInterestRate = computed(() => {
-  if (model.annualInterestRate) {
-    return model.annualInterestRate / 12;
-  } else {
-    return 0
-  }
-})
 
-let parse =  (input: string) => {
-        const nums = input.replace(/,/g, '').trim()
-        if (/^\d+(\.(\d+)?)?$/.test(nums)) return Number(nums)
-        return nums === '' ? null : Number.NaN
-      }
 
-    let  format = (value: number | null) => {
-        if (value === null) return ''
-        return value.toLocaleString('en-US')
-      }
+let parse = (input: string) => {
+  const nums = input.replace(/,/g, '').trim()
+  if (/^\d+(\.(\d+)?)?$/.test(nums)) return Number(nums)
+  return nums === '' ? null : Number.NaN
+}
+
+let format = (value: number | null) => {
+  if (value === null) return ''
+  return value.toLocaleString('en-US')
+}
 
 // 计算等额本金贷款
 let equalPrincipalPayments = computed(() => {
   if (model.years && model.loanAmount && model.annualInterestRate) {
-    return calculateEqualPrincipalLoan(model.years, monthlyInterestRate.value, model.loanAmount)
+    return calculateEqualPrincipalLoan(model.years, model.annualInterestRate, model.loanAmount)
   } else {
-    return { totalMonths: 0, totalPayment: 0, totalInterest: 0 };
+    return { monthlyPayment: 0, totalPayment: 0, totalInterest: 0 };
   }
 })
 
 // 等额本息贷款
 let equalInstallmentPayments = computed(() => {
   if (model.years && model.loanAmount && model.annualInterestRate) {
-    return calculateEqualInstallmentLoan(model.years, monthlyInterestRate.value, model.loanAmount)
+    console.log(calculateLoan(model.years, model.annualInterestRate, model.loanAmount), 11111)
+    return calculateLoan(model.years, model.annualInterestRate, model.loanAmount)
   } else {
-    return { totalMonths: 0, totalPayment: 0, totalInterest: 0 };
+    return { monthlyPayment: 0, totalPayment: 0, totalInterest: 0 };
   }
 })
 
@@ -81,33 +109,47 @@ let equalInstallmentPayments = computed(() => {
 // 计算等额本金贷款
 function calculateEqualPrincipalLoan(years, annualInterestRate, loanAmount) {
   const totalMonths = years * 12;
-  const monthlyInterestRate = new Big(annualInterestRate).div(12);
-  const monthlyPrincipal = new Big(loanAmount).div(totalMonths);
+  const monthlyInterestRate = _.divide(annualInterestRate, 12);
+  const monthlyPayment = _.divide(loanAmount, totalMonths);
 
-  let totalInterest = new Big(0);
+  let totalPayment = 0;
+  let totalInterest = 0;
+
   for (let i = 0; i < totalMonths; i++) {
-    totalInterest = totalInterest.plus(
-      new Big(loanAmount).minus(new Big(i).times(monthlyPrincipal)).times(monthlyInterestRate)
-    );
+    const remainingPrincipal = loanAmount - (i * monthlyPayment);
+    const interestPayment = remainingPrincipal * monthlyInterestRate;
+    const principalPayment = monthlyPayment - interestPayment;
+
+    totalPayment += monthlyPayment;
+    totalInterest += interestPayment;
   }
 
-  const totalPayment = new Big(loanAmount).plus(totalInterest);
-
-  return { totalMonths, totalPayment: totalPayment.toFixed(2), totalInterest: totalInterest.toFixed(2) };
+  return {
+    monthlyPayment: _.round(monthlyPayment, 2),
+    totalInterest: _.round(totalInterest, 2),
+    totalPayment: _.round(totalPayment, 2)
+  };
 }
 
 // 计算等额本息贷款
-function calculateEqualInstallmentLoan(years, annualInterestRate, loanAmount) {
+function calculateLoan(years, annualInterestRate, loanAmount) {
   const totalMonths = years * 12;
-  const monthlyInterestRate = new Big(annualInterestRate).div(12);
-  const monthlyPayment = new Big(loanAmount).times(monthlyInterestRate)
-    .div(new Big(1).minus(new Big(1).div(new Big(1).plus(monthlyInterestRate).pow(totalMonths))));
+  const monthlyInterestRate = _.divide(annualInterestRate, 12);
+  console.log(monthlyInterestRate, "月利率")
+  
+  const compoundFactor = Math.pow(1 + monthlyInterestRate, totalMonths);
+  console.log(compoundFactor, "复利因子")
+  
+  const monthlyPayment = _.multiply(_.multiply(loanAmount, monthlyInterestRate), _.divide(compoundFactor, _.subtract(compoundFactor, 1)));
 
-  const totalPayment = monthlyPayment.times(totalMonths);
-  const totalInterest = totalPayment.minus(loanAmount);
-
-  return { totalMonths, totalPayment: totalPayment.toFixed(2), totalInterest: totalInterest.toFixed(2) };
+  console.log('%c%s', 'color: #aa00ff', monthlyPayment, '计算每月还款额')
+  return {
+    monthlyPayment: monthlyPayment,
+    totalInterest: _.subtract(_.multiply(monthlyPayment, totalMonths), loanAmount),
+    totalPayment: _.multiply(monthlyPayment, totalMonths)
+  };
 }
+
 
 
 </script>
