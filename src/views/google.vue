3dfe60aa8b9f940e3e5b667e2d4619f5e94022df
webkubor <webkubor@163.com>
2024-01-16 10:07:45 +0800

    fixed 修复验证后没有输出的问题

--- src/views/google.vue
+++ src/views/google.vue
@@ -1,50 +1,52 @@
 <template>
-    <div id="captcha" style="margin:10px;"></div>
-  </template>
+   <div id="captcha" style="margin:10px;"></div>
+</template>
   
-  <script setup>
-  import { ref,onMounted } from 'vue';
-  
-  let codeFlag = ref(false);
-  let captchaId = null;
-  onMounted(() => {
-  handleLoadGoogleCaptcha();
+<script setup>
+import { ref, onMounted, reactive } from 'vue';
+
+let register = reactive({
+   captchaCode: ''
+})
+let codeFlag = ref(false);
+let captchaId = null;
+onMounted(() => {
+   handleLoadGoogleCaptcha();
 });
-  function handleLoadGoogleCaptcha() {
-    codeFlag.value = false;
-    captchaId = grecaptcha.render("captcha", {
+function handleLoadGoogleCaptcha() {
+   codeFlag.value = false;
+   captchaId = grecaptcha.render("captcha", {
       "sitekey": '6Lc_OlEpAAAAABJA5kqj1SM0S3vo_tmbKNHuor48',
       "theme": "light",
       "callback": handleCaptchaCallback,
       "expired-callback": handleCaptchaExpired,
       "error-callback": handleCaptchaError,
       "hl": 'zh-CN',
-    });
-  }
-  
-  function handleCaptchaCallback(val) {
-    if (val != null) {
-      register.captchaCode = val;
-      console.log(val,'handleCaptchaCallback');
-      codeFlag.value = true;
-    }
-  }
-  
-  function handleCaptchaExpired() {
-    register.captchaCode = "";
-    codeFlag.value = false;
-  }
-  
-  function handleCaptchaError() {
-    register.captchaCode = "";
-    grecaptcha.reset(captchaId);
-    codeFlag.value = false;
-  }
-  </script>
-  <style>
-#captcha {
-    border: 1px solid #ccc;
+   });
 }
 
+function handleCaptchaCallback(val) {
+   if (val) {
+      register.captchaCode = val;
+      console.log(val, 'handleCaptchaCallback');
+      codeFlag.value = true;
+   }
+}
+
+function handleCaptchaExpired() {
+   register.captchaCode = "";
+   codeFlag.value = false;
+}
+
+function handleCaptchaError() {
+   register.captchaCode = "";
+   grecaptcha.reset(captchaId);
+   codeFlag.value = false;
+}
+</script>
+<style>
+#captcha {
+   border: 1px solid #ccc;
+}
 </style>
   
