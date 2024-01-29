1eb4ed6f40a248127131c72b40b25a8c97e2d900
webkubor <webkubor@163.com>
2024-01-29 14:27:48 +0800

    feat 暂时提交处理王冠类型

--- src/views/wings/index.vue
+++ src/views/wings/index.vue
@@ -1,10 +1,18 @@
 <template>
     <div class="bg">
         <div class="flame">
+            <img class="avactor" src="https://xboxcsgo.oss-cn-hongkong.aliyuncs.com/files/test/avactor.png" alt="">
+            <div class="name-title">
+                King
+            </div>
             <img class="red" src="https://xboxcsgo.oss-cn-hongkong.aliyuncs.com/files/test/fly-open-removebg-preview.png">
             <div class="light"></div>
         </div>
         <div class="flame">
+            <img class="avactor" src="https://xboxcsgo.oss-cn-hongkong.aliyuncs.com/files/test/avactor.png" alt="">
+            <!-- <div class="name-title">
+                King
+            </div> -->
             <img src="https://xboxcsgo.oss-cn-hongkong.aliyuncs.com/files/test/king.png">
             <div class="light">
             </div>
@@ -34,6 +42,18 @@ initBackgroundMusic()
 </script>
 
 <style lang="scss" scoped>
+@keyframes masked-animation {
+    0% {
+        background-position: 0 0;
+    }
+
+    100% {
+        background-position: -100% 0;
+    }
+}
+
+@import url("https://fonts.googleapis.com/css2?family=Bangers&family=Homemade+Apple&family=Sacramento&display=swap");
+
 .bg {
     min-height: 100vh;
     width: 100vw;
@@ -44,7 +64,7 @@ initBackgroundMusic()
     flex-direction: column;
     color: #ffffff;
     padding-bottom: 20vh;
-
+    padding: 0 20px 30vh;
 }
 
 
@@ -59,11 +79,46 @@ initBackgroundMusic()
         text-align: center;
     }
 
+    .avactor {
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        position: absolute;
+        top: 80px;
+        z-index: 10;
+        left: 50%;
+        transform: translateX(-50%);
+        filter: drop-shadow(0 0 10px yellow);
+    }
+
+    .name-title {
+        top: 180px;
+        position: relative;
+        text-align: center;
+        filter: drop-shadow(0 0 1em #F0000E);
+        font-size: 40px;
+        color: yellow;
+        z-index: 50;
+        font-family: "Bangers", cursive;
+        width: 300px;
+        height: 200px;
+        position: absolute;
+        left: 50%;
+        margin-left: -150px;
+        background-image: -webkit-linear-gradient(left, #F0000E, #c2ff66 10%, #ff6a00 20%, #F0000E 30%, #CCCCFF 40%, #ff6a00 50%, #CCCCFF 60%, #CC00CC 70%, #F0000E 80%, #ff6a00 90%, #c2ff66 100%);
+        -webkit-text-fill-color: transparent;
+        /* 将字体设置成透明色 */
+        -webkit-background-clip: text;
+        /* 裁剪背景图，使文字作为裁剪区域向外裁剪 */
+        -webkit-background-size: 200% 100%;
+        -webkit-animation: masked-animation 4s linear infinite;
+    }
+
     .light {
         width: 500px;
         height: 600px;
         position: absolute;
-        top: -200px;
+        top: -265px;
         left: -50px;
         background: url(https://xboxcsgo.oss-cn-hongkong.aliyuncs.com/files/test/light.png) no-repeat center;
         background-size: contain;
@@ -112,9 +167,11 @@ initBackgroundMusic()
     margin-top: -20px;
 
     .flame {
-        width: 300px;
-        margin: 0 120px;
+        width: 45%;
+        margin: 0 20px;
 
+        img {
+            width: 200px;
+        }
     }
-}
-</style>
+}</style>
