8aa4ad9ba250330f797e270f0f708b4027381ad7
webkubor <webkubor@163.com>
2024-01-31 18:15:28 +0800

    feat 暂存代码

--- src/views/xbox/openBox.vue
+++ src/views/xbox/openBox.vue
@@ -1,68 +1,40 @@
 <template>
     <div class="open-box-page">
-        <div class="product-box">
-            <img v-if="showStart" src="@/assets/xbox/box/end.png" alt="Product Image">
-            <video ref="video" loop src="@/assets/xbox/box/move.mp4"></video>
-        </div>
-        <img class="start-botton" src="@/assets/xbox/box/button.png" alt="Product Image" @click="onOpen">
-        <h1 class="btn-text">点击按钮开始</h1>
+      <div class="product-box" :class="{ 'rotate': showAnimate }">
+        2
+      </div>
+      <img class="start-botton" src="@/assets/xbox/box/button.png" alt="Product Image" @click="onOpen">
     </div>
-</template>
-<script setup>
-
-import { useMusic } from "@/hooks/useMusic"
-import processClick from '@/assets/click/process.mp3';
-import { ref, onMounted } from "vue";
-let video = ref(null)
-let showStart = ref(true)
-const { onClickBox, playAudio } = useMusic()
-
-onMounted(() => {
-    video.value.style.display = "none"
-
-})
-// function onOpen() {
-//     onClickBox()
-//     showStart.value = false
-//     video.value.style.display = "block"
-//     setTimeout(() => {
-//         video.value.play()
-//         let clickAudio = new Audio(processClick);
-//         playAudio(clickAudio)
-
-//         setTimeout(() => {
-//             showStart.value = true
-//             video.value.style.display = "none"
-//         }, 6000)
-//     }, 500);
-
-// }
-
-
-async function onOpen() {
+  </template>
+  
+  <script setup>
+  import { useMusic } from "@/hooks/useMusic";
+  import processClick from '@/assets/click/process.mp3';
+  import { ref, onMounted } from "vue";
+  
+  const { onClickBox, playAudio } = useMusic();
+  const showAnimate = ref(false);
+  const animationDuration = 6 * 1000; // 6 seconds in milliseconds
+  
+  onMounted(() => {});
+  
+  async function onOpen() {
     onClickBox();
-    showStart.value = false;
-    const videoElement = video.value;
-    videoElement.style.display = "block";
-
+    if (showAnimate.value) return;
+    showAnimate.value = true;
+    console.log(showAnimate.value, 222222);
     await new Promise(resolve => setTimeout(resolve, 500));
-
-    videoElement.play();
     const clickAudio = new Audio(processClick);
     playAudio(clickAudio);
-
-    await new Promise(resolve => setTimeout(resolve, 6000));
-
-    showStart.value = true;
-    videoElement.style.display = "none";
-}
-
-</script>
-<style lang="scss" scoped>
-.open-box-page {
-    // 添加样式
-    background: url(https://xboxcsgo.oss-cn-hongkong.aliyuncs.com/files/xboxcsgo/red-bg.webp) no-repeat left top;
-    background-size: 100% 100%;
+    await new Promise(resolve => setTimeout(resolve, animationDuration));
+    showAnimate.value = false;
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  .open-box-page {
+    background: url(@/assets/xbox/box/bg4.png) no-repeat center center;
+    background-size: contain;
     color: #ffffff;
     font-weight: 600;
     height: 100vh;
@@ -72,50 +44,38 @@ async function onOpen() {
     text-align: center;
     flex-flow: column;
     align-items: center;
-
+    background-color: #040607;
+  
     .product-box {
-        cursor: pointer;
-        display: flex;
-        flex-flow: column wrap;
-        width: 700px;
-
-        img {
-            width: 100%;
-            border-radius: 20px;
-            position: relative;
-            z-index: 2;
-            transition: all 0.5s;
-        }
-
-        video {
-            width: 100%;
-            position: relative;
-            z-index: 1;
-            transition: all 0.5s;
-        }
+      cursor: pointer;
+      display: flex;
+      flex-flow: column wrap;
+      border: 1px solid #ffffff;
+      width: 500px;
+      height: 500px;
+      border-radius: 50%;
+      animation: rotate linear infinite; /* 添加这行 */
+      animation-duration: animationDuration; /* 添加这行 */
+      transition: transform cubic-bezier(0.4, 0, 0.2, 1) 1s;
     }
-
+  
+    .rotate {
+      transform: rotate(360deg);
+    }
+  
     .start-botton {
-        position: relative;
-        width: 150px;
-        filter: brightness(120%) drop-shadow(0 0 1em #F0000E);
-        border-radius: 50%;
-        top: -80px;
-        cursor: pointer;
-        z-index: 3;
-
-
-        &:hover {
-            border: 2px solid #F0000E;
-        }
+      position: relative;
+      width: 150px;
+      border-radius: 50%;
+      top: 200px;
+      cursor: pointer;
+      z-index: 3;
+  
+      &:hover {
+        border: 3px solid #f0000e;
+        filter: brightness(120%) drop-shadow(0 0 2em #f0000e);
+      }
     }
-
-    .btn-text {
-        position: relative;
-        font-size: 18px;
-        color: orangered;
-        top: -80px;
-    }
-
-}
-</style>
+  }
+  </style>
+  
