365377ef19dbb0088d77efdf82f9fe46e099d136
webkubor <webkubor@163.com>
2024-05-30 17:34:11 +0800

    feat  切换背景色

--- src/views/loading/index.vue
+++ src/views/loading/index.vue
@@ -1,10 +1,9 @@
 <template>
-  <n-card title="loading样式测试">
-    <n-button>
-      背景切换
-    </n-button>
-  </n-card>
-  <n-space vertical>
+    <n-card title="背景切换">
+      <n-color-picker v-model:value="model.color" :actions="['confirm']" @confirm="handleConfirm"></n-color-picker>
+    </n-card>
+
+  <n-space vertical style="margin-top: 30px;">
     <n-space justify="space-around">
       <circleLoading />
       <flowerLoading />
@@ -14,6 +13,7 @@
 
     <n-space justify="space-around">
       <Boundce />
+      <Flow />
     </n-space>
   </n-space>
 
@@ -21,10 +21,39 @@
 
 
 </template>
-<script setup>
+<script lang="ts" setup>
 import circleLoading from './components/circle.vue';
 import flowerLoading from './components/flower.vue'
 import waitingLoading from './components/waiting.vue'
 import waitingLoading2 from './components/waiting2.vue'
 import Boundce from './components/bounce.vue'
+import Flow from "./components/flow.vue"
+import { reactive } from 'vue';
+const model = reactive({
+  color: '#161B29'
+})
+
+function handleConfirm(value: string) {
+  console.log(value)
+  const root = document.documentElement;
+  root.style.setProperty('--webkubor-loading-color', value ? value : '#161B29');
+}
+
+
+
 </script>
+<style lang="scss" scoped>
+:root {
+  --webkubor-loading-color: #161B29;
+}
+
+.iphone15-border {
+  width: 430px;
+  height: 732px;
+  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.2),
+    0 4px 6px rgba(0, 0, 0, 0.15);
+  border-radius: 20px;
+  filter: drop-shadow(0px 1px 1em $primary-color);
+  background-color: var(--webkubor-loading-color);
+}
+</style>
