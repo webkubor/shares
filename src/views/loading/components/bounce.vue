e498823c65155c0783a6b5178a7ba2ebcebff1c6
webkubor <webkubor@163.com>
2024-05-30 15:48:16 +0800

    提交Loading样式

---
+++ src/views/loading/components/bounce.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="container iphone15-border">
+        <div class="ground">
+            <div class="ball"></div>
+        </div>
+    </div>
+
+</template>
+<style lang="scss" scoped>
+.container {
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.ground {
+    position: relative;
+    width: 80%;
+    height: 10px;
+    background: linear-gradient(to right, rgba(52, 152, 219, 0) 0%, rgba(52, 152, 219, 1) 50%, rgba(52, 152, 219, 0) 100%);
+    box-shadow: 0 0 20px rgba(52, 152, 219, 1);
+    animation: glow 4s infinite;
+}
+
+.ball {
+    width: 50px;
+    height: 50px;
+    background: radial-gradient(circle, #3498db, #2980b9);
+    border-radius: 50%;
+    position: absolute;
+    bottom: 10px; /* Align with the ground */
+    left: calc(50% - 25px);
+    animation: bounce 2s infinite ease-in-out, colorChange 4s infinite, sizeChange 2s infinite;
+    filter: drop-shadow(0 0 10px #3498db);
+}
+
+@keyframes bounce {
+    0%, 20%, 50%, 80%, 100% {
+        transform: translateY(0);
+    }
+    40% {
+        transform: translateY(-150px);
+    }
+    60% {
+        transform: translateY(-75px);
+    }
+}
+
+@keyframes colorChange {
+    0% {
+        background: radial-gradient(circle, #80bbe2, #4fc9eb);
+        filter: drop-shadow(0 0 10px #3498db);
+    }
+    50% {
+        background: radial-gradient(circle, #3c4de7, #1d8bd5);
+        filter: drop-shadow(0 0 20px #2bcff0);
+    }
+    100% {
+        background: radial-gradient(circle, #80bbe2, #4fc9eb);
+        filter: drop-shadow(0 0 10px #3498db);
+    }
+}
+
+@keyframes sizeChange {
+    0%, 100% {
+        width: 50px;
+        height: 50px;
+    }
+    50% {
+        width: 60px;
+        height: 60px;
+    }
+}
+
+@keyframes glow {
+    0%, 100% {
+        box-shadow: 0 0 20px rgba(52, 152, 219, 1);
+    }
+    50% {
+        box-shadow: 0 0 40px rgba(52, 152, 219, 1);
+    }
+}
+</style>
--- src/views/loading/index.vue
+++ src/views/loading/index.vue
@@ -1,16 +1,24 @@
 <template>
- <n-card  title="loading样式测试">
-  <n-button>
-    背景切换
-  </n-button>
-</n-card>
+  <n-card title="loading样式测试">
+    <n-button>
+      背景切换
+    </n-button>
+  </n-card>
+  <n-space vertical>
+    <n-space justify="space-around">
+      <circleLoading />
+      <flowerLoading />
+      <waitingLoading />
+      <waitingLoading2 />
+    </n-space>
 
-<n-space justify="space-around">
-  <circleLoading/>
-  <flowerLoading/>
-  <waitingLoading/>
-  <waitingLoading2/>
-</n-space>
+    <n-space justify="space-around">
+      <Boundce />
+    </n-space>
+  </n-space>
+
+
+
 
 </template>
 <script setup>
@@ -18,4 +26,5 @@ import circleLoading from './components/circle.vue';
 import flowerLoading from './components/flower.vue'
 import waitingLoading from './components/waiting.vue'
 import waitingLoading2 from './components/waiting2.vue'
+import Boundce from './components/bounce.vue'
 </script>
