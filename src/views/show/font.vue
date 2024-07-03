9212c2833930434ca7da8e6bc449e0d3821b0e4c
webkubor <webkubor@163.com>
2024-07-03 11:34:35 +0800

    Feat 提交基本素材

--- src/views/show/font.vue
+++ src/views/show/font.vue
@@ -4,7 +4,7 @@
         <n-card title="彩云字体设计">
             <div class="type-chinese1" @click="selectFont('type-chinese1')"
                 :class="{ selected: selectedFont === 'type-chinese1' }">
-                <p class="vertical-text">
+                <p>
                     {{ textContent }}
                 </p>
             </div>
@@ -12,7 +12,7 @@
         <n-card title="白鸽天行体">
             <div class="type-chinese2" @click="selectFont('type-chinese2')"
                 :class="{ selected: selectedFont === 'type-chinese2' }">
-                <p class="vertical-text">
+                <p>
                     {{ textContent }}
                 </p>
             </div>
@@ -20,13 +20,13 @@
         <n-card title="Aa墨韵行书">
             <div class="type-chinese3" @click="selectFont('type-chinese3')"
                 :class="{ selected: selectedFont === 'type-chinese3' }">
-                <p class="vertical-text">
+                <p>
                     {{ textContent }}
                 </p>
             </div>
         </n-card>
     </n-space>
-    <n-space>
+    <n-space class="container-1">
         <div v-for="(book, index) in books" :key="index" class="book"
             :class="[book.bg, { selected: selectedBook === index + 1 }]" @click="selectBook(index + 1)">
             <div :class="selectedFont">
@@ -37,16 +37,35 @@
                     <span class="big">
                         百晓生
                     </span>
-                    
+
                 </div>
                 <div class="author vertical-text">苏梦城 著</div>
-                <!-- <div class="author2">苏梦城&nbsp;著</div> -->
                 <p class="vertical-text other">
                     {{ textContent }}
                 </p>
             </div>
         </div>
     </n-space>
+    <h2>魔刀青衣封面选择</h2>
+    <n-space class="container-2">
+        <div v-for="(book, index) in books" :key="index" class="book"
+            :class="[book.bg, { selected: selectedBook === index + 1 }]" @click="selectBook(index + 1)">
+            <div :class="selectedFont">
+                <div class="title">
+                        魔刀青衣
+                
+
+                </div>
+              
+                <div class="author ">酒歌狂
+                    
+                    <img class="blood" src="@/assets/book/blood2.png" alt="">著</div>
+
+                <img class="logo" src="@/assets/book/logo.png">
+            </div>
+        </div>
+    </n-space>
+
 </template>
 
 <script setup>
@@ -81,69 +100,144 @@ function selectBook(bookIndex) {
     transition: border 0.3s;
 }
 
-.author2 {
-    position: absolute;
-    bottom: 15px;
-    left: 50%;
-    transform: translateX(-50%);
-    font-size: 50px;
-}
 
-.author {
-    position: absolute;
-    top: 70px;
-    left: 110px;
-    font-size: 30px;
-}
 
-.other {
-    position: absolute;
-    top: 30px;
-    left: 20px;
-    font-size: 30px;
-}
 
-.title {
-    position: absolute;
-    top: 30px;
-    right: -10px;
+.container-1 {
 
-    .big {
-        font-size: 150px;
-        position: relative;
-        top: -60px;
-        text-shadow: 
-    0 0 1px black,
-    0 0 1px black,
-    0 0 1px black,
-    0 0 1px black,
-    0 0 1px black;
+    .author {
+        position: absolute;
+        top: 70px;
+        left: 110px;
+        font-size: 30px;
     }
 
-    span {
-        position: relative;
-        font-size: 60px;
+    .other {
+        position: absolute;
+        top: 30px;
+        left: 20px;
+        font-size: 30px;
+    }
+
+    .title {
+        position: absolute;
+        top: 30px;
+        right: -10px;
+
+        .big {
+            font-size: 150px;
+            position: relative;
+            top: -60px;
+            text-shadow:
+                0 0 1px black,
+                0 0 1px black,
+                0 0 1px black,
+                0 0 1px black,
+                0 0 1px black;
+        }
+
+        span {
+            position: relative;
+            font-size: 60px;
+        }
+    }
+
+    .bg1 {
+        background: url(@/assets/book/w-1.png) no-repeat;
+        background-size: contain;
+    }
+
+    .bg2 {
+        background: url(@/assets/book/w-2.png) no-repeat;
+        background-size: contain;
+    }
+
+    .bg3 {
+        background: url(@/assets/book/w-3.png) no-repeat;
+        background-size: contain;
+    }
+
+    .bg4 {
+        background: url(@/assets/book/w-4.png) no-repeat;
+        background-size: contain;
+    }
+
+}
+
+@supports (-webkit-text-stroke: 1px black) or (text-stroke: 1px black) {
+    .title {
+        -webkit-text-stroke: 1px #FFFFFF; /* 白色描边 */
+        text-stroke: 1px #FFFFFF; /* 白色描边 */
     }
 }
 
-.bg1 {
-    background: url(@/assets/book/1.png) no-repeat;
-    background-size: contain;
-}
 
-.bg2 {
-    background: url(@/assets/book/2.png) no-repeat;
-    background-size: contain;
-}
+.container-2 {
+    color: #ffffff;
+    text-align: center;
+    padding-bottom: 50px;
+    .book {
+        padding-top: 40px;
+        position: relative;
+    }
 
-.bg3 {
-    background: url(@/assets/book/3.png) no-repeat;
-    background-size: contain;
-}
+    .title {
+        font-size: 140px;
+        color: #FF4500;  /* 较亮的红色字体，鲜明且突出 */
+        text-shadow: 4px 4px 8px rgba(0, 0, 0, 0.7);  /* 更明显的文字阴影 */
+        letter-spacing: 2px;  /* 字符间距 */
+        text-transform: uppercase;  /* 全部大写，增加气势 */
+        filter: drop-shadow(0 0 2px #000000);  /* 文字阴影 */
+        position: relative;
+        // img {
+        //     width: 300px;
+        //     position: absolute;
+        //     top: -20px;
+        //     right: 200px;
+        //     display: none;
+        // }
 
-.bg4 {
-    background: url(@/assets/book/4.png) no-repeat;
-    background-size: contain;
+    }
+    .blood {
+        position: absolute;
+        top: 30px;
+        left: 70%;
+        transform: translateX(-50%);
+        width: 100px;
+    }
+    .author {
+        font-size: 40px;
+        font-weight: normal;  /* 正常字体粗细 */
+        margin-top: 10px;  /* 书名和作者名之间的间距 */
+        color: #FF6347;  /* 略浅的红色字体，增加对比 */
+        filter: drop-shadow(0 0 4px #000000);  /* 文字阴影 */
+    }
+    .bg1 {
+        background: url(@/assets/book/m-1.png) no-repeat;
+        background-size: cover;
+    }
+
+    .bg2 {
+        background: url(@/assets/book/m-2.png) no-repeat;
+        background-size: cover;
+    }
+
+    .bg3 {
+        background: url(@/assets/book/m-3.png) no-repeat;
+        background-size: cover;
+    }
+
+    .bg4 {
+        background: url(@/assets/book/m-4.png) no-repeat;
+        background-size: cover;
+    }
+    .logo {
+        height: 30px;
+        position: absolute;
+        bottom: 20px;
+        left: 50%;
+        transform: translateX(-50%);
+    }
 }
 
 
@@ -159,7 +253,6 @@ function selectBook(bookIndex) {
 .type-chinese1,
 .type-chinese2,
 .type-chinese3 {
-    font-size: 100px;
     transition: border 0.3s;
 }
 
@@ -177,6 +270,5 @@ function selectBook(bookIndex) {
 
 .selected {
     border: 2px solid #333333;
-    /* 选中时亮起的边框颜色 */
 }
 </style>
