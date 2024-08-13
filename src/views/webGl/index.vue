48eeab6b6d257189149fbf2b7462e8f283cef6c7
webkubor <webkubor@163.com>
2024-08-13 16:43:36 +0800

    Feat 提交

--- src/views/webGl/index.vue
+++ src/views/webGl/index.vue
@@ -3,11 +3,13 @@
     <h2>
         webGL 
     </h2>
+    <div>
+        <div id="#glcanvas"></div>
+    </div>
 </template>
 <script setup>
 import {useWebGl} from '@/hooks/useWebGl'
 import { onMounted } from 'vue';
-
 const {init} = useWebGl()
 onMounted(()=>{
     init()
