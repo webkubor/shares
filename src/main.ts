6ad5fe3e33fa225f07cba31b212b27cf398d1c02
webkubor <webkubor@163.com>
2024-08-22 14:59:46 +0800

    feat 移除无用部分依赖

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import router from './router'
 import i18n from '@/plugins/i18n'
 import copy from "@/directives/copy";
 import ripple from "@/directives/ripple/index";
-import globalComponents from "@/components";
 import "@icon-park/vue-next/styles/index.css";
 
 createApp(App)
@@ -12,5 +11,4 @@ createApp(App)
 .use(i18n)
 .use(ripple)
 .use(router)
-.use(globalComponents)
 .mount('#app')
