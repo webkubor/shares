11d74ba8920999da32a2bbab05b740f25fd7fc88
webkubor <webkubor@163.com>
2024-11-12 15:02:13 +0800

    feat  提交配置

--- components.d.ts
+++ components.d.ts
@@ -8,7 +8,6 @@ declare module 'vue' {
     NButton: typeof import('naive-ui')['NButton']
     NCard: typeof import('naive-ui')['NCard']
     NCheckbox: typeof import('naive-ui')['NCheckbox']
-    NCollapseTransition: typeof import('naive-ui')['NCollapseTransition']
     NColorPicker: typeof import('naive-ui')['NColorPicker']
     NConfigProvider: typeof import('naive-ui')['NConfigProvider']
     NDialogProvider: typeof import('naive-ui')['NDialogProvider']
--- vite.config.ts
+++ vite.config.ts
@@ -26,21 +26,7 @@ export default defineConfig({
     },
   },
   build: {
-    chunkSizeWarningLimit: 1500,
-    rollupOptions: {
-      output: {
-        chunkFileNames: 'chunks/[name]-[hash].js', //设置非入口 chunk 的文件名格式。这里表示将非入口chunk的JS文件输出到chunks目录下，文件名由[name]（chunk的名字）和[hash]（哈希值）组成，这样可以保证每次修改内容后输出的文件名带有不同的哈希值，有利于浏览器缓存更新
-        assetFileNames: 'assets/[name]-[hash][extname]', //置非JavaScript和CSS的其他静态资源（如图片、字体等）的输出路径和文件名格式，同样带有[name]和[hash]，以及原始资源的扩展名[extname]。
-        manualChunks: {
-          vendor: ['vue', 'vue-router', 'vue-i18n'],
-          lib: ['dayjs', 'flyio', 'lodash', 'plyr', 'mathjs'],
-          naive: ['naive-ui'],
-          phosphor: ['@phosphor-icons/vue'],
-          g2plot: ['@antv/g2plot'],
-          echarts: ['echarts'],
-        }
-      }
-    }
+    chunkSizeWarningLimit: 1500
   },
   plugins: [
     vueTools(),
@@ -65,13 +51,13 @@ export default defineConfig({
         "background_color": "#242424",
         icons: [
           {
-            "src": "/shares/logo.svg",
+            "src": "/logo.svg",
             "sizes": "192x192",
             "type": "image/svg+xml",
             purpose: 'any maskable'
           },
           {
-            "src": "/shares/logo.svg",
+            "src": "/logo.svg",
             "sizes": "512x512",
             "type": "image/svg+xml",
             purpose: 'any maskable'
