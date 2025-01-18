a75479aadad966d3aed01f07ebbfd31b17b98e3a
webkubor <webkubor@163.com>
2025-01-18 12:26:54 +0800

    Feat 暂时提交无关配置

--- src/views/show/login/index.vue
+++ src/views/show/login/index.vue
@@ -1,5 +1,4 @@
 <template>
-	<!-- <inputPage/> -->
 	<Login3 />
 	<Login5 />
 	<Login1 />
@@ -8,5 +7,4 @@
 import Login3 from "./components/login3.vue"
 import Login1 from "./components/login1.vue"
 import Login5 from "./components/login5.vue"
-import inputPage from "./components/inputPage.vue";
 </script>
