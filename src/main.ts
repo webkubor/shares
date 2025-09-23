778e586ffff42972d6d1751a17e35b1c5e3b1be2
webkubor <webkubor@163.com>
2025-09-23 18:03:08 +0800

    feat 项目减负

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import i18n from '@/plugins/i18n'
 import copy from "@/directives/copy";
 import ripple from "@/directives/ripple/index";
 import InputHistory from "@/directives/inputHistory";
@@ -12,7 +11,6 @@ import 'core-js/stable';
 createApp(App)
 
 .use(copy)
-.use(i18n)
 .use(ripple)
 .use(InputHistory)
 .use(router)
